7612551f1835e703987e86ed5fefa08b93538451
leejiyeon <sky75013@naver.com>
2025-09-11 12:46:59 +0900

    test: 003 delete

--- src/main/java/com/back/global/db/Sql.java
+++ src/main/java/com/back/global/db/Sql.java
@@ -57,4 +57,18 @@ public class Sql {
             throw new RuntimeException(e);
         }
     }
+
+    public int delete() {
+        try (Connection conn = simpleDb.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sb.toString().trim())) {
+
+            for (int i = 0; i < params.size(); i++) {
+                pstmt.setObject(i + 1, params.get(i));
+            }
+
+            return pstmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }

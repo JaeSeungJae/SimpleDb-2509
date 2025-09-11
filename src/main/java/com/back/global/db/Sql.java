e7793fe537752d59a7a106e6dc6bd1c64a2f38dc
leejiyeon <sky75013@naver.com>
2025-09-11 16:58:52 +0900

    test: 004 selectRows

--- src/main/java/com/back/global/db/Sql.java
+++ src/main/java/com/back/global/db/Sql.java
@@ -1,9 +1,7 @@
 package com.back.global.db;
 
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Sql {
     private final SimpleDb simpleDb;
@@ -55,6 +53,38 @@ public class Sql {
              PreparedStatement pstmt = conn.prepareStatement(sb.toString())) {
 
             return simpleDb.executeUpdate(pstmt, sb.toString(), params);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public List<Map<String, Object>> selectRows() {
+        try (Connection conn = simpleDb.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sb.toString(), ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY)) {
+
+            for (int i = 0; i < params.size(); i++) {
+                pstmt.setObject(i + 1, params.get(i));
+            }
+
+            try (ResultSet rs = pstmt.executeQuery()) {
+                List<Map<String, Object>> results = new ArrayList<>();
+
+                ResultSetMetaData metaData = rs.getMetaData();
+                int columnCount = metaData.getColumnCount();
+
+                while (rs.next()) {
+                    Map<String, Object> row = new HashMap<>();
+
+                    for (int i = 1; i <= columnCount; i++) {
+                        String columnName = metaData.getColumnLabel(i);
+                        Object value = rs.getObject(i);
+                        row.put(columnName, value);
+                    }
+                    results.add(row);
+                }
+
+                return results;
+            }
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }

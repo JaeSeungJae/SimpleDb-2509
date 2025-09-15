405374e80290bcc600903dbffd22c6e7dfdb07e7
leejiyeon <sky75013@naver.com>
2025-09-15 12:42:57 +0900

    test: 017 multi threading

--- src/main/java/com/back/global/db/SimpleDb.java
+++ src/main/java/com/back/global/db/SimpleDb.java
@@ -12,6 +12,8 @@ public class SimpleDb {
     private final String password;
     private boolean devMode = false;
 
+    private final ThreadLocal<Connection> threadLocalConnection = new ThreadLocal<>();
+
     public SimpleDb(String host, String user, String password, String dbName) {
         this.url = "jdbc:mysql://" + host + ":3306/" + dbName + "?serverTimezone=UTC";
         this.user = user;
@@ -22,14 +24,35 @@ public class SimpleDb {
         return new Sql(this, devMode);
     }
 
+    Connection getConnection() throws SQLException {
+        Connection conn = threadLocalConnection.get();
+        try {
+            if (conn == null || conn.isClosed()) {
+                conn = DriverManager.getConnection(url, user, password);
+                threadLocalConnection.set(conn);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("DB 연결 실패", e);
+        }
+        return conn;
+    }
+
     public void run(String query, Object... params) {
         new QueryExecutor(this, devMode).executeUpdate(query, List.of(params), false);
     }
 
-    Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(url, user, password);
-    }
-
     public void close() {
+        Connection conn = threadLocalConnection.get();
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                throw new RuntimeException("Connection close 실패", e);
+            } finally {
+                threadLocalConnection.remove();
+            }
+        } else {
+            throw new RuntimeException("Connection이 존재하지 않습니다.");
+        }
     }
 }
